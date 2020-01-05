7fee90d4d57cd25d186f379e131674b25fbc13d2
Raffael Meyer <raffael@alyf.de>
2020-01-05 18:20:50 +0100

    feat: first working version

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
+## Frappe Flask-Test
+
+https://frappe.io/flask-test
+
 Activate venv:
 
 ```bash
 source ./env/bin/activate
 ```
 
+Run app:
+
+```bash
+cd flask-test
+flask run
+```
--- flask-test/app.py
+++ flask-test/app.py
@@ -1,47 +1,105 @@
 from flask import Flask
+from flask import request
+from flask import abort, redirect, url_for
 from flask import render_template
+import sqlite3
+
 app = Flask(__name__)
 
-LOCATIONS = [
-    {
-        "id": "1",
-        "title": "Warehouse Leipzig"
-    },
-    {
-        "id": "2",
-        "title": "Warehouse Berlin"
-    }
-]
+def dict_factory(cursor, row):
+    """Convert SQL row to Python dict."""
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
-PRODUCTS = [
-    {
-        "id": "1",
-        "title": "Apple"
-    },
-    {
-        "id": "2",
-        "title": "Pear"
-    }
-]
+conn = sqlite3.connect('flask-test.db')
+conn.row_factory = dict_factory
+c = conn.cursor()
+c.execute("""
+CREATE TABLE IF NOT EXISTS
+    Location (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title
+    )
+""")
+c.execute("""
+    CREATE TABLE IF NOT EXISTS
+        Product (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title,
+            location
+        )
+""")
+c.execute("""
+    CREATE TABLE IF NOT EXISTS
+        ProductMovement (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            from_location INTEGER,
+            to_location INTEGER NOT NULL,
+            product_id INTEGER NOT NULL,
+            qty
+        )
+""")
+conn.commit()
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/product')
-def get_product_list():
-    return render_template("list.html", items=PRODUCTS)
+@app.route('/<doc_type>', methods=['GET'])
+def get_list(doc_type):
+    return render_template('list.html', items=_get_list(doc_type), doc_type=doc_type)
 
-@app.route('/product/<product_id>')
-def get_product(product_id):
-    product = list(filter(lambda location: location["id"]==product_id, PRODUCTS))[0]
-    return render_template("single.html", doc=product)
+@app.route('/<doc_type>', methods=['POST'])
+def create_doc(doc_type):
+    if request.method == 'POST':
+        title = request.form['title']
+        if title:
+            _insert_doc(doc_type, title)
 
-@app.route('/location')
-def get_location_list():
-    return render_template("list.html", items=LOCATIONS)
+    return redirect(url_for("get_list", doc_type=doc_type))
 
-@app.route('/location/<location_id>')
-def get_location(location_id):
-    location = list(filter(lambda location: location["id"]==location_id, LOCATIONS))[0]
-    return render_template("single.html", doc=location)
+@app.route('/<doc_type>/<int:doc_id>', methods=['GET'])
+def get_doc(doc_type, doc_id):
+    doc = _get_doc(doc_type, doc_id)
+    return render_template('single.html', doc=doc, doc_type=doc_type, locations=_get_list("Location"))
+
+@app.route('/<doc_type>/<int:doc_id>/<method>', methods=['POST'])
+def run_method(doc_type, doc_id, method):
+    if doc_type == 'Product' and method == 'move':
+        destination = request.form['destination']
+        if destination:
+            _move(doc_id, destination)
+    
+    return redirect(url_for("get_doc", doc_type=doc_type, doc_id=doc_id))
+
+def _move(product_id, destination_id):
+    print("Moving product {} to location {}".format(product_id, destination_id))
+    doc = _get_doc('Product', product_id)
+
+    source_id = None
+    source = doc.get('location')
+    if isinstance(source, dict):
+        source_id = source.get('id')
+
+    c.execute('INSERT INTO ProductMovement VALUES (?, ?, ?, ?, ?, ?)', (None, None, source_id, destination_id, product_id, 0))
+    c.execute('UPDATE Product SET location = "%s" WHERE id = %d' % (destination_id, product_id))
+    conn.commit()
+
+def _get_list(doc_type):
+    doc_list = c.execute('SELECT * FROM %s' % doc_type)
+    return doc_list.fetchall()
+
+def _get_doc(doc_type, doc_id):
+    results = c.execute('SELECT * FROM %s WHERE id = %d' % (doc_type, doc_id))
+    doc = results.fetchone()
+    if doc.get('location'):
+        doc['location'] = _get_doc("Location", int(doc.get('location')))
+
+    return doc
+
+def _insert_doc(doc_type, title):
+    c.execute('INSERT INTO %s (title) VALUES (?)' % doc_type, [title])
+    conn.commit()
