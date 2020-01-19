57f363ef3202da7d7a228b01510f854496de5731
Raffael Meyer <raffael@alyf.de>
2020-01-19 15:15:44 +0100

    feat(list): respond as json

--- flask_test/views/listview.py
+++ flask_test/views/listview.py
@@ -1,21 +1,22 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, Response, request, render_template)
+import json
 from flask_test import db
 from flask_test.schema import FORM_SCHEMA
+from flask_test.utils import row_as_json, list_as_json
 
 bp = Blueprint('list', __name__)
 
 @bp.route('/resource/<doc_type>', methods=['GET'])
 def get(doc_type):
-    if doc_type == "Report":
-        return render_template(
-            'report.html',
-            items=db.get_list(doc_type),
-            doc_type=doc_type
-        )
+    data = db.get_list(doc_type)
+
+    if 'application/json' in request.headers.get('accept'):
+        response = json.dumps(list_as_json(data))
+        return Response(response, mimetype="application/json")
 
     return render_template(
         'list.html',
-        items=db.get_list(doc_type),
+        items=data,
         doc_type=doc_type,
         fields=FORM_SCHEMA[doc_type],
         get_options=db.get_list
