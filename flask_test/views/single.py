6d2f89e7d89c315cef7160d9854e147797dac62b
Raffael Meyer <raffael@alyf.de>
2020-01-19 15:18:00 +0100

    feat(single):  respond as json

--- flask_test/views/single.py
+++ flask_test/views/single.py
@@ -1,9 +1,16 @@
-from flask import (Blueprint, render_template)
+import json
+from flask import (Blueprint, request, render_template)
 from flask_test.db import Document
+from flask_test.utils import row_as_json
 
 bp = Blueprint('single', __name__)
 
 @bp.route('/resource/<doc_type>/<doc_id>', methods=['GET'])
 def get(doc_type, doc_id):
     doc = Document(doc_type, doc_id)
-    return render_template('single.html', doc=doc.get(), doc_type=doc_type)
+    data = doc.get()
+
+    if 'application/json' in request.headers.get('accept'):
+        return json.dumps(row_as_json(data))
+
+    return render_template('single.html', doc=data, doc_type=doc_type)
