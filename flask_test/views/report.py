35d6f336c93b98bf7ae49c4097c8429de5dce7c2
Raffael Meyer <raffael@alyf.de>
2020-01-19 15:16:34 +0100

    feat(Report): respond as json

--- flask_test/views/report.py
+++ flask_test/views/report.py
@@ -1,12 +1,20 @@
-from flask import (Blueprint, render_template)
+import json
+from flask import (Blueprint, Response, request, render_template)
 from flask_test import db
+from flask_test.utils import list_as_json
 
 bp = Blueprint('report', __name__)
 
 @bp.route('/Report', methods=['GET'])
 def get():
+    data = db.get_list("Report")
+
+    if 'application/json' in request.headers.get('accept'):
+        response = json.dumps(list_as_json(data))
+        return Response(response, mimetype="application/json")
+
     return render_template(
         'report.html',
-        items=db.get_list("Report"),
+        items=data,
         doc_type="Report"
     )
